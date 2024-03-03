04f706907469ab99c985d852d48e5d029afe8cd8
Dat Do <dtdat201202@gmail.com>
2024-03-03 23:01:17 +0700

    category api

---
+++ app/routes/category.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const category = require("../controllers/category.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(category.findAll)
+    .post(category.create)
+    .delete(category.deleteAll);
+    
+router.route("/:id")
+    .get(category.findOne)
+    .put(category.update)
+    .delete(category.delete);
+
+module.exports = router;
