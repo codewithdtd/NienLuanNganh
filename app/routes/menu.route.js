36e79e1cf03084edbabb7dabbd27245e6e9ce38e
Dat Do <dtdat201202@gmail.com>
2024-03-04 11:55:29 +0700

    Dieu chinh category api

--- app/routes/menu.route.js
+++ app/routes/menu.route.js
@@ -3,16 +3,21 @@ const menu = require("../controllers/menu.controller");
 
 const router = express.Router();
 
+router.route("/category")
+    .get(menu.findAllCategory)
+    .post(menu.createCategory)
+    .delete(menu.deleteAllCategory);
+    
+router.route("/category/:id")
+    .get(menu.findOneCategory)
+    .put(menu.updateCategory)
+    .delete(menu.deleteCategory);
+
 router.route("/")
     .get(menu.findAll)
     .post(menu.create)
     .delete(menu.deleteAll);
-    
-// router.route("/menu/category")
-//     .get(menu.findCategory)
-//     .post(menu.createCategory)
-//     .put(menu.updateCategory)
-//     .delete(menu.deleteCategory);
+
 
 router.route("/:id")
     .get(menu.findOne)
